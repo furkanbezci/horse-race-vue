a41d665e26c92c61dd774e9f74683ba874723e60
furkanbezci <baburfurkan1@gmail.com>
2025-08-18 01:30:20 +0300

    refactor: constants file changed

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,28 +1,52 @@
+
 export const horseNames = [
-    { name: "Fırtına", color: "red" },
-    { name: "Kasırga", color: "orange" },
-    { name: "Şimşek MekQuin", color: "olive" },
-    { name: "Uçurtma Uçuran", color: "brown" },
-    { name: "5'inci Günün Şafağı", color: "green" },
-    { name: "Gün Batımı", color: "magenta" },
-    { name: "Pazar Kahvaltısı", color: "grey" },
-    { name: "Koşar Kasa", color: "lavender" },
-    { name: "Hayalet", color: "white" },
-    { name: "Tayfun", color: "khaki" },
-    { name: "Gambit", color: "teal" },
-    { name: "Yaralı Yalnız", color: "silver" },
-    { name: "İyi, kötü, hızlı", color: "pink" },
-    { name: "Sanrı gören", color: "purple" },
-    { name: "Zamanı Büken", color: "beige" },
-    { name: "Izabel", color: "lime" },
-    { name: "Baştankara", color: "maroon" },
-    { name: "Tamahkar cüce", color: "indigo" },
-    { name: "I am the danger", color: "crimson" },
-    { name: "Walter Ride", color: "plum" },
+    "Fırtına",
+    "Kasırga",
+    "Şimşek MekQuin",
+    "Uçurtma Uçuran",
+    "5'inci Günün Şafağı",
+    "Gün Batımı",
+    "Pazar Kahvaltısı",
+    "Koşar Kasa",
+    "Hayalet",
+    "Tayfun",
+    "Gambit",
+    "Yaralı Yalnız",
+    "İyi, kötü, hızlı",
+    "Sanrı gören",
+    "Zamanı Büken",
+    "Izabel",
+    "Baştankara",
+    "Tamahkar cüce",
+    "I am the danger",
+    "Walter Ride",
+
+]
+export const horseColors = [
+    "red",
+    "orange",
+    "olive",
+    "brown",
+    "green",
+    "magenta",
+    "grey",
+    "lavender",
+    "white",
+    "khaki",
+    "teal",
+    "silver",
+    "pink",
+    "purple",
+    "beige",
+    "lime",
+    "maroon",
+    "indigo",
+    "crimson",
+    "plum",
 ]
 export const distances = [
     1200, 1400, 1600, 1800, 2000, 2200
 ]
 export const roundLabels = ['1st', '2nd', '3rd', '4th', '5th', '6th']
 export const raceAreaLengthInPixel = 470
-export const deltaTime= 0.012
+export const deltaTime = 0.012
