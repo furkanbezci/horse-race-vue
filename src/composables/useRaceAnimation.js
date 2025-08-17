77ed341015a9d31dbc07904284aa5393383f9b0d
furkanbezci <baburfurkan1@gmail.com>
2025-08-18 01:31:05 +0300

    refactor: composables refactor

--- src/composables/useRaceAnimation.js
+++ src/composables/useRaceAnimation.js
@@ -5,25 +5,41 @@ export function useRaceAnimation() {
   const store = useStore()
   let animationId
 
+  const minSpeed = 50
+  const maxSpeed = 100
+
+  const randomDeltaSpeedCalculator = () => {
+
+    if (Math.random() < 0.8) {
+      return 0
+    }
+    const delta = Math.random() < 0.5 ? -4 : 4
+    return delta
+
+  }
+
   const animate = () => {
     animationId = requestAnimationFrame(animate)
-
 
     if (!store.state.isRaceActive) return
 
     const round = store.state.activeRound
     const pxPerMeter = raceAreaLengthInPixel / store.state.raceScheduleData[round].distance
-
     const horses = store.state.raceScheduleData[round].horses
 
     for (let index = 0; index < horses.length; index++) {
+
       const condition = horses[index].condition
+      horses[index].speed = horses[index].speed !== 0 ? Math.max(minSpeed, Math.min(maxSpeed, horses[index].speed + randomDeltaSpeedCalculator())) : 0
       const currentSpeed = horses[index].speed * condition / 100 * pxPerMeter
+
       horses[index].position += currentSpeed * deltaTime
 
       if (horses[index].position >= raceAreaLengthInPixel && !horses[index].finished) {
+        console.log(horses[index].position >= raceAreaLengthInPixel)
         horses[index].speed = 0
         horses[index].finished = true
+        horses[index].position = raceAreaLengthInPixel
         store.state.raceResults[round].horses.push(horses[index])
         const allFinished = horses.every(v => v.finished)
         if (allFinished) {
